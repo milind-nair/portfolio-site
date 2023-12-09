46316984bdb6afb8f6e12c00f14c1dc48a948fd5
Milind Nair <nairmilind3@gmail.com>
2023-12-09 17:56:32 +0530

    added responsive app drawer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import PersistentDrawerLeft from "./components/ResponsiveDrawer";
+import { createTheme, ThemeProvider, Button } from "@mui/material";
+
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <PersistentDrawerLeft />
+        <Button onClick={toggleDarkMode}>
+          {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </Button>
+      </ThemeProvider>
+    </>
   );
 }
 
